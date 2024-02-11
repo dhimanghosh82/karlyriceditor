7178e8ebe8a401b96f8d486440a079c8e95fdbd5
dhimanghosh82 <159672398+dhimanghosh82@users.noreply.github.com>
2024-02-10 21:27:36 -0500

    Update textrenderer.h

--- src/textrenderer.h
+++ src/textrenderer.h
@@ -22,6 +22,9 @@
 
 #include <QFont>
 #include <QColor>
+#include <QString>
+#include <QStringBuilder>
+#include <QTextBoundaryFinder>
 
 #include "lyricsrenderer.h"
 #include "lyricsevents.h"
@@ -31,134 +34,91 @@ class Project;
 
 class TextRenderer : public LyricsRenderer
 {
-	public:
-        enum VerticalAlignment
-        {
-            // default = 0
-            VerticalBottom = 0,
-            VerticalMiddle = 1,
-            VerticalTop = 2
-        };
+public:
+    enum VerticalAlignment
+    {
+        VerticalBottom = 0,
+        VerticalMiddle = 1,
+        VerticalTop = 2
+    };
 
-		TextRenderer( int width, int height );
+    TextRenderer(int width, int height);
 
-		// Sets the lyrics to render. Resets all previously set params to defaults except setDisplaySize.
-		void	setLyrics( const Lyrics& lyrics );
+    void setLyrics(const Lyrics &lyrics);
+    void setRenderFont(const QFont &font);
+    void setColorBackground(const QColor &color);
+    void setColorTitle(const QColor &color);
+    void setColorToSing(const QColor &color);
+    void setColorSang(const QColor &color);
+    void setPreambleData(unsigned int height, unsigned int timems, unsigned int count);
+    void setTitlePageData(const QString &artist, const QString &title, const QString &userCreatedBy, unsigned int msec);
+    void setColorAlpha(int alpha);
+    void setDefaultVerticalAlign(VerticalAlignment align);
+    void forceCDGmode();
+    void setDurations(unsigned int before, unsigned int after);
+    void setPrefetch(unsigned int prefetch);
 
-		// Lyrics data to render, overrides defaults from settings
-        void	setRenderFont( const QFont& font );
-		void	setColorBackground( const QColor& color );
-		void	setColorTitle( const QColor& color );
-		void	setColorToSing( const QColor& color );
-		void	setColorSang( const QColor& color );
-		void	setPreambleData( unsigned int height, unsigned int timems, unsigned int count );
-		void	setTitlePageData( const QString& artist, const QString& title, const QString& userCreatedBy, unsigned int msec ); // duration = 0 - no title, default
-		void	setColorAlpha( int alpha ); // 0 - 255
-        void    setDefaultVerticalAlign( VerticalAlignment align );
+    virtual int update(qint64 timing);
 
-		// Force CD+G rendering mode (no anti-aliasing)
-		void	forceCDGmode();
+    static bool checkFit(const QSize &imagesize, const QFont &font, const QString &text);
 
-		// Typically lyrics are shown a little before they are being sung, and kept after they end.
-		// This function overrides default before (5000ms) and after (1000ms) lengths
-		void	setDurations( unsigned int before, unsigned int after );
+    int autodetectFontSize(const QSize &imagesize, const QFont &font);
+    bool verifyFontSize(const QSize &imagesize, const QFont &font);
 
-		// Some formats (like CD+G) draw lyrics pretty slow, which means if the lyrics screens
-		// change immediately, at the time the next page is being drawn it is already sung.
-		// This parameter changes the prefetch, meaning if its value is 1000, the new lyrics block
-		// is always shown at least 1000ms before it is being sing, even if it is necessary to cut the
-		// old block earlier. Default is zero.
-		void	setPrefetch( unsigned int prefetch );
+private:
+    struct LyricBlockInfo
+    {
+        qint64 timestart;
+        qint64 timeend;
+        QString text;
+        QMap<qint64, unsigned int> offsets;
+        QMap<unsigned int, QString> colors;
+        QMap<unsigned int, int> fonts;
+        int verticalAlignment;
+    };
 
-		// Draw a new lyrics image
-		virtual int	update( qint64 timing );
+    QVector<LyricBlockInfo> m_lyricBlocks;
+    bool m_forceRedraw;
+    bool m_cdgMode;
+    QColor m_colorBackground;
+    QColor m_colorTitle;
+    QColor m_colorToSing;
+    QColor m_colorSang;
+    QFont m_renderFont;
+    unsigned int m_preambleHeight;
+    unsigned int m_preambleLengthMs;
+    unsigned int m_preambleCount;
+    unsigned int m_beforeDuration;
+    unsigned int m_afterDuration;
+    unsigned int m_prefetchDuration;
+    int m_preambleTimeLeft;
+    int m_lastDrawnPreamble;
+    qint64 m_lastSungTime;
+    bool m_drawPreamble;
+    int m_lastBlockPlayed;
+    int m_lastPosition;
+    LyricsEvents m_lyricEvents;
+    int m_currentAlignment;
 
-		// Checks if a line or block fits into the requested image.
-		static	bool checkFit( const QSize& imagesize, const QFont& font, const QString& text );
+    // Function to normalize Bengali text using NFC
+    QString normalizeBengali(const QString &input)
+    {
+        QString normalizedText = input.normalized(QString::NormalizationForm_C);
 
-		// Autodetects the largest font size to fit all lyrics into a specific image size.
-		int		autodetectFontSize( const QSize& imagesize, const QFont& font );
+        // Additional processing if needed
 
-		// Verifies that all lyrics could be rendered into a specific image size using the provided font
-		bool	verifyFontSize( const QSize& imagesize, const QFont& font );
+        return normalizedText;
+    }
 
-    private:
-		// Returns the lyrics bounding box for a line or for paragraph using the font specified,
-		// or the default font if not specified
-		QRect	boundingRect( int blockid, const QFont& font );
-
-		void	init();
-		void	prepareEvents();
-		int		lyricForTime( qint64 tickmark, int * sungpos );
-		QString	titleScreen() const;
-		void	fixActionSequences( QString& block );
-		void	drawLyrics( int blockid, int pos, const QRect& boundingRect );
-		void	drawPreamble();
-		void	drawBackground( qint64 timing );
-
-	private:
-		// Lyrics to render
-		typedef struct
-		{
-			qint64	timestart;
-			qint64	timeend;
-
-			// Text is for the whole block, with all special characters stripped down
-			QString	text;
-
-			// Text offsets in block per specific time
-			QMap< qint64, unsigned int > offsets;
-
-			// Per-character color changes for following (non-sung) characters in the block.
-			// if none, the default color is used
-			QMap< unsigned int, QString > colors;
-
-			// Per-character font size changes for following (non-sung) characters in the block.
-			// if none, the default color is used
-			QMap< unsigned int, int > fonts;
-
-			int	verticalAlignment;
-
-		} LyricBlockInfo;
-
-		QVector< LyricBlockInfo >	m_lyricBlocks;
-
-		// Compile a single line
-		void	compileLine( const QString& line, qint64 starttime, qint64 endtime, LyricBlockInfo * binfo, bool *intitle );
-
-		// True if the image must be redrawn even if lyrics didn't change
-		bool					m_forceRedraw;
-
-        // True if CD+G mode - no antialiasing
-        bool                    m_cdgMode;
-
-		// Rendering params
-		QColor					m_colorBackground;
-		QColor					m_colorTitle;
-		QColor					m_colorToSing;
-		QColor					m_colorSang;
-		QFont					m_renderFont;
-		unsigned int			m_preambleHeight;	// how tall the preamble square should be; 0 - no preamble
-		unsigned int			m_preambleLengthMs;	// maximum time the preamble is shown
-		unsigned int			m_preambleCount;	// how many preamble squares to draw for m_preambleLengthMs
-		unsigned int			m_beforeDuration;
-		unsigned int			m_afterDuration;
-		unsigned int			m_prefetchDuration;
-
-		// Handling the preamble stuff
-		int						m_preambleTimeLeft;	// Time left to show the current preamble - 5000 ... 0
-		int						m_lastDrawnPreamble; // Timing when the last time the preamble changed
-		qint64					m_lastSungTime;
-		bool					m_drawPreamble;
-
-		int						m_lastBlockPlayed;
-		int						m_lastPosition;
-
-		// Background events
-		LyricsEvents			m_lyricEvents;
-
-		// Vertical alignment
-		int						m_currentAlignment;
+    QRect boundingRect(int blockid, const QFont &font);
+    void init();
+    void prepareEvents();
+    int lyricForTime(qint64 tickmark, int *sungpos);
+    QString titleScreen() const;
+    void fixActionSequences(QString &block);
+    void drawLyrics(int blockid, int pos, const QRect &boundingRect);
+    void drawPreamble();
+    void drawBackground(qint64 timing);
 };
 
 #endif // TEXTRENDERER_H
